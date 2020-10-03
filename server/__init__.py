0f12b05cfdf41808c42954db96af62114bbc619e
Nugetzrul3 <52128634+Nugetzrul3@users.noreply.github.com>
2020-10-03 11:30:15 +1000

    Update to sugarchain-api master

--- server/__init__.py
+++ server/__init__.py
@@ -4,15 +4,17 @@ from flask_restful import Api
 from flask_cors import CORS
 from flask import Flask
 from server import config
+import eventlet
 import time
+
+eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config["SECRET_KEY"] = config.secret
 cache = Cache(config={"CACHE_TYPE": "simple"})
-sio = SocketIO(app, cors_allowed_origins="*")
+sio = SocketIO(app, cors_allowed_origins="*", async_mode = "eventlet", message_queue = "redis://")
 cache.init_app(app)
-api = Api(app)
 CORS(app)
 
 start_time = time.monotonic()
@@ -20,8 +22,8 @@ socket_counter = 0
 rest_counter = 0
 
 watch_addresses = {}
-connections = 0
 subscribers = {}
+connections = 0
 thread = None
 mempool = []
 
@@ -33,4 +35,4 @@ from server import rest
 esplora.init(app)
 routes.init(app)
 socket.init(sio)
-rest.init(api)
+rest.init(app)
